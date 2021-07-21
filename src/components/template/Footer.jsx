c79427e51eb309a027aa7b8ce4fec6915e33ede3
CaioVinicius10 <45971241+CaioVinicius10@users.noreply.github.com>
2021-07-21 11:29:41 -0300

    first commit

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className='footer'>
+        <span>
+             <strong><span>Sistema de cadastro</span></strong>
+            <i className></i>
+            <strong></strong>
+        </span>
+    </footer>
